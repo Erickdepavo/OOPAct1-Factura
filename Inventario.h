fdeade25a2eb7d5185e66846fd9f93f523ddd8e0
Erick Daniel Padilla <erickdelpavo@gmail.com>
2021-05-13 14:03:42 -0600

    [ENHANCEMENT] Creado Inventario.h

---
+++ Inventario.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+class Inventario {
+    public:
+        string cve_articulo;
+        string descripcion;
+        double precio;
+
+        void addInventario(Inventario inventarios[], Inventario inventario)
+};
